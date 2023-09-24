28cc31716477d76050ef778ce39941234de8f352
deepshikhalcb049 <lcb2021049@iiitl.ac.in>
2023-09-25 00:12:31 +0530

    new commit

--- components/cards/page.js
+++ components/cards/page.js
@@ -6,7 +6,7 @@ export default function Cards(props) {
     console.log(c)
     return (
         <div className="cardsout bg-[#FFF] mt-[90px] p-[12px]">
-            <p className="cardtext mt-[35px] text-[#002A54]">{props.details[0]}</p>
+            <p className="cardtext text-[24px] mt-[35px] text-[#002A54]">{props.details[0]}</p>
                 <Image src={props.details[1]} width={props.details[3].width} height={props.details[3].height} className={c}></Image>
                 <p className="cardtextb text-[#002A54] mt-[20px]">{props.details[2]}</p>
                 <Image src='/Arrow 1.png' width={98} height={0} className="float-right flex mr-[20px]"></Image>
--- components/news/page.js
+++ components/news/page.js
@@ -1,9 +1,19 @@
-export default function News(){
-return(
-    <div className="h-[780px] bg-[#ED1B24] news mt-[70px]">
-{/* <div className="cardsout bg-[#FFF]">
+export default function News() {
+    return (
+        <div className="h-[780px] bg-[#ED1B24] news mt-[70px] flex">
+           {
+            [...Array(3)].map((e)=>{
+                return( <div className="cardsout bg-[#FFF] ml-[50px] relative top-[50px] ">
+                <div className="h-[295px] w-[427px] bgphoto ">
 
-</div> */}
-    </div>
-)
+                </div>
+                <p className="cardtext text-[24px] mt-[10px] ml-[10px]">World Shipping Council Releases
+                    Containers Lost At Sea Report</p>
+                <p className="cardtextb mt-[12px] ml-[10px]">Network of strategically based sites located on port (within 1km of container terminals) and off-port (within 15kms of capital city industrial zones) </p>
+                <p className="cardtext text-[20px] text-[#ED1B24] float-right mr-[20px] mt-[-20px]">Read More</p>
+            </div>)
+            })
+           }
+        </div>
+    )
 }
